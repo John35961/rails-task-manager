dcd07e0e34a7bcbd6589845c1ed2dbf1107e0a7b
Jean Guillet <jean.guillet1@gmail.com>
2022-05-28 23:25:06 +0200

    adding 'resources' to follow REST routes logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-
-  get 'tasks/:id', to: 'tasks#show', as: :task
-
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-
-  post 'tasks', to: 'tasks#create'
-
-  patch 'tasks/:id', to: 'tasks#update'
-
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources 'tasks'
 end
