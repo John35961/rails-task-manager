0e25050c40a8a5c17327405166aeb643029cebd2
Jean Guillet <jean.guillet1@gmail.com>
2022-05-29 15:30:34 +0200

    adding pundit authorization and scope to CRUD routes, based on user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include Pundit::Authorization
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
   def after_sign_in_path_for(*)
     tasks_path
   end
@@ -9,5 +14,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,32 +2,40 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
 
   def index
-    @tasks = Task.all
-    @done = Task.where(completed: 1)
+    @tasks = policy_scope(Task)
+    @done = @tasks.where(completed: 1)
     @progress = (@done.length.to_f / @tasks.length) * 100
   end
 
-  def show; end
+  def show
+    authorize @task
+  end
 
   def new
     @task = Task.new
+    authorize @task
   end
 
   def create
     @task = Task.new(task_params)
     @task.user = current_user
+    authorize @task
     @task.save
     redirect_to tasks_path(@task)
   end
 
-  def edit; end
+  def edit
+    authorize @task
+  end
 
   def update
+    authorize @task
     @task.update(task_params)
     redirect_to tasks_path
   end
 
   def destroy
+    authorize @task
     @task.destroy
     redirect_to tasks_path
   end
