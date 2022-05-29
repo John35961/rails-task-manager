1798c083b27b637492a67a2fe75cee340143f83e
Jean Guillet <jean.guillet1@gmail.com>
2022-05-29 11:49:26 +0200

    adding priority to Task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_29_094629) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_29_094813) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_29_094629) do
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
     t.integer "user_id", null: false
+    t.integer "priority"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
