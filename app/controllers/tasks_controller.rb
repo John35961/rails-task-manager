c7f17d13b92283c436d9377d82bcd72b77d092d8
Jean Guillet <jean.guillet1@gmail.com>
2022-06-15 16:05:51 +0200

    adding 'priority' in authorized params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :details, :completed)
+    params.require(:task).permit(:title, :details, :completed, :priority)
   end
 
   def set_task
