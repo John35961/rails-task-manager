6c5af6e759169b694501fb4dc0b3c7cf70caabe1
Jean Guillet <jean.guillet1@gmail.com>
2022-06-17 16:01:39 +0200

    conditionnal title coloration based on checkbox state

--- app/javascript/controllers/tasks_controller.js
+++ app/javascript/controllers/tasks_controller.js
@@ -2,14 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tasks"
 export default class extends Controller {
-  static targets = [ "completed" ]
+  static targets = [ "title", "completed" ]
 
   connect() {
     this.csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
+    const tasks = this.completedTargets;
+    tasks.forEach((task) => {
+      if (task.checked) {
+        this.titleTarget.classList.toggle("opacity-50");
+      };
+    });
   }
 
   toggle(event) {
     event.preventDefault();
+    this.titleTarget.classList.toggle("opacity-50");
     let formData = new FormData();
     formData.append("task[completed]", this.completedTarget.checked);
     fetch(this.data.get("update-url"), {
