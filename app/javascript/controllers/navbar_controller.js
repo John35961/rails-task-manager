a4dd82197a7a77d578e7e5e0502a6793441b52b4
Jean Guillet <jean.guillet1@gmail.com>
2022-06-21 16:34:54 +0200

    injecting minisearch in navbar, on scroll

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
+  static targets = [ "minisearch" ]
   updateNavbar () {
     if (window.scrollY >= 50) {
       this.element.classList.remove('navbar-transparent');
@@ -10,5 +11,11 @@ export default class extends Controller {
       this.element.classList.remove('navbar-primary');
       this.element.classList.add('navbar-transparent');
     };
+    if (window.scrollY >= 125) {
+      this.minisearchTarget.classList.remove('d-none');
+      console.log(this.minisearchTarget.children[0].children[2].classList.add('text-light'));
+    } else {
+      this.minisearchTarget.classList.add('d-none');
+    }
   };
 }
